5b1f5db9b7969ce0052ceeb65a38b630ea362893
Greg Dobler <greg.dobler@gmail.com>
2013-11-23 18:05:12 -0500

    updated

--- py/__init__.py
+++ py/__init__.py
@@ -6,3 +6,4 @@ from dst13.py.dst_read_bin import *
 from dst13.py.dst_window_pix import *
 from dst13.py.dst_window_labels import *
 from dst13.py.dst_light_curves import *
+from dst13.py.dst_register import *
