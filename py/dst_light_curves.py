e3bb7f1ad29455e185cd328bd830860df70a1757
Greg Dobler <greg.dobler@gmail.com>
2013-11-23 14:44:41 -0500

    extract image brightnesses

--- py/dst_light_curves.py
+++ py/dst_light_curves.py
@@ -2,6 +2,7 @@ import os
 import pickle as pkl
 import numpy as np
 from .dst_window_labels import *
+from .dst_io import *
 
 
 # -------- 
@@ -16,13 +17,18 @@ class LightCurves():
                  wpath=os.environ['DST_WRITE']):
         """ Make night time light curves for start and end times """
 
+        # -- utilities
+        bord = 20
+        nrow = 2160
+        ncol = 4096
+
         # -- get the full file list
         fopen = open(os.path.join(wpath,'filelist.pkl'),'rb')
         fl    = pkl.load(fopen)
         fopen.close()
 
         # -- take a time slice
-        paths, files, times = fl.time_slice(start, end)
+        cls.paths, cls.files, cls.times = fl.time_slice(start, end)
 
         # -- get the window labels
         labels = window_labels(hand=True)
@@ -30,18 +36,49 @@ class LightCurves():
         labvec = labels[wpix].flatten()
         sind   = np.argsort(labvec)
         labsrt = labvec[sind]
-        lbound = [0] + list(np.where((labsrt-np.roll(labsrt,1))==1)[0]) + 
-                 [labsrt.size]
+        lbound = list(np.where((labsrt-np.roll(labsrt,1))==1)[0])
+        lbound = [0] + lbound + [labsrt.size]
 
         # -- initialize the light curves
-        ntime = len(times)
-        npix  = labels[labels>0].size
+        cls.ntime = len(cls.times)
+        cls.nwin  = len(lbound) - 1
+        cls.lcs   = np.zeros([cls.nwin,cls.ntime,3])
+        cls.std   = np.zeros([cls.nwin,cls.ntime,3])
+        cls.err   = np.zeros([cls.nwin,cls.ntime,3])
+        cls.bse   = np.zeros([cls.nwin,cls.ntime,3])
 
+        # -- initialize color vectors
+        red = np.zeros(sind.size)
+        grn = np.zeros(sind.size)
+        blu = np.zeros(sind.size)
+        img = np.zeros([nrow-2*bord,ncol-2*bord,3]).astype(np.uint8)
 
-# get a list of images (night time)
+        # -- loop through images
+        for itime, (p,f,t) in enumerate(zip(cls.paths,cls.files,cls.times)):
+            print("DST_LIGHT_CURVES: extracting window brightnesses for " + 
+                  "file {0} of {1}".format(itime+1,len(cls.times)))
 
-# read in the image, extract just the brightness of window pixels,
-# average over pixels with a given label, compute the bootstrap
-# (remove 2) error, compute the standard deviation
+            img[:,:,:] = read_raw(f,p)[bord:-bord,bord:-bord,:]
 
-# times, amplitudes, errors
+            red[:] = ((img[:,:,0])[wpix])[sind].astype(np.float)
+            grn[:] = ((img[:,:,1])[wpix])[sind].astype(np.float)
+            blu[:] = ((img[:,:,2])[wpix])[sind].astype(np.float)
+
+            for iwin in range(cls.nwin):
+                ilo = lbound[iwin]
+                ihi = lbound[iwin+1]
+                rtn = np.sqrt(float(ihi-ilo+1))
+
+                cls.lcs[iwin,itime,0] = red[ilo:ihi].mean()
+                cls.lcs[iwin,itime,1] = grn[ilo:ihi].mean()
+                cls.lcs[iwin,itime,2] = blu[ilo:ihi].mean()
+
+                cls.std[iwin,itime,0] = red[ilo:ihi].std()
+                cls.std[iwin,itime,1] = grn[ilo:ihi].std()
+                cls.std[iwin,itime,2] = blu[ilo:ihi].std()
+
+                cls.std[iwin,itime,0] = cls.std[iwin,itime,0]/rtn
+                cls.std[iwin,itime,1] = cls.std[iwin,itime,1]/rtn
+                cls.std[iwin,itime,2] = cls.std[iwin,itime,2]/rtn
+
+        return
