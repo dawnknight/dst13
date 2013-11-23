0a325a18c300d45ab80cdb0962e1882339f1e298
Greg Dobler <greg.dobler@gmail.com>
2013-11-23 18:15:35 -0500

    full convolution based registration

--- py/dst_register.py
+++ py/dst_register.py
@@ -1,50 +1,59 @@
 import os,pickle,multiprocessing
 import numpy as np
-from scipy.signal import fftconvolve, correlate2d
+from scipy.signal import fftconvolve
 from .dst_io import *
-import pdb
 
 # -------- 
 #  Register a raw image
 #
 #  2013/11/23 - Written by Greg Dobler (CUSP/NYU)
 # -------- 
-def register(inpath='', infile='', outpath='', outfile='', start='', 
-                 end='', multi=False):
+def register(inpath=None, infile=None, outpath=None, outfile=None, start=None, 
+             end=None, multi=False):
+
     """ Register a single image or all images between some start and
     end time.  Pixels shifts for registration are written to a pickled
     dictionary in which the keys are the filenames and the values are
     a list or [row,col] pixel shifts."""
 
 
-    # utilities
+    # -- utilities
     bord  = 20
     bvec  = np.arange(-bord,bord+1)
     blen  = bvec.size
     nside = 800 # npix/side of a postage stamp
     reg   = (900, 1600, 1700, 2400) # (ul row, ul col, ll row, ll col)
-    regsm = (reg[0]+bord,reg[1]+bord,reg[2]-bord,reg[3]-bord)
 
 
-    # set the reference frame (registering off of the green image)
+    # -- set the reference frame (registering off of the green image)
     rpath = os.path.join(os.environ['DST_DATA'],'11/15/19.03.48')
     rfile = 'oct08_2013-10-25-175504-182135.raw'
-    ref   = 1.0*read_raw(rfile,rpath)[regsm[0]:regsm[2],regsm[1]:regsm[3],1]
+    ref   = 1.0*read_raw(rfile,rpath)[reg[0]:reg[2],reg[1]:reg[3],1]
     ref  -= ref.mean()
     ref  /= ref.std()
 
 
-    # get the nighttime slice
-    paths = [rpath]
-    files = ['oct08_2013-10-25-175504-182136.raw']
+    # -- get the nighttime slice
+    if start!=None:
+        print("DST_REGISTER: registering all images from " + 
+              "{0} to {1}".format(start,end))
+
+        fopen = open(os.path.join(os.environ['DST_WRITE'],'filelist.pkl'))
+        fl    = pickle.load(fopen)
+        fopen.close()
+
+        paths, files, times = fl.time_slice(start,end)
+    else:
+        paths = [inpath]
+        files = [infile]
 
 
-    # initialize the correlation matrix and dictionary
+    # -- initialize the correlation matrix and dictionary
     cc_mat = np.zeros([len(files),ref.shape[0],ref.shape[1]])
     cc_dic = {}
 
 
-    # loop through the files and calculate the (sub-)correlation matrix
+    # -- loop through the files and calculate the (sub-)correlation matrix
     nproc  = 1 if not multi else multi
     nfiles = len(files)
     dind   = 1 if nproc==1 \
@@ -53,33 +62,38 @@ def register(inpath='', infile='', outpath='', outfile='', start='',
 
     def reg_subset(conn,paths,files,verbose=False):
 
-        # initialize the correlation sub-matrix dictionary
+        # -- initialize the postage stamp, correlation, & sub-matrix dictionary
+        stm        = np.zeros([reg[2]-reg[0],reg[3]-reg[1]])
         cc_sub_mat = np.zeros([len(files),ref.shape[0],ref.shape[1]])
         cc_sub_dic = {}
 
-        # loop through files
+        # -- loop through files
         for i,(p,f) in enumerate(zip(paths,files)):
             if verbose:
                 print("DST_REGISTER: " + 
                       "calculating correlation coefficient for file " + 
                       "{0} of {1}".format(i+1,len(files)))
 
-            img = 1.0*read_raw(f,p)[reg[0]:reg[2],reg[1]:reg[3],1]
-
-            # shift and find correlation
-            print("DST_REGISTER: convolving")
-
-            stm  = img[bord:-bord,bord:-bord]
-            stm -= stm.mean()
-            stm /= stm.std()
+            # -- shift and find correlation
+            stm[:,:] = 1.0*read_raw(f,p)[reg[0]:reg[2],reg[1]:reg[3],1]
+            stm     -= stm.mean()
+            stm     /= stm.std()
 
             cc_sub_mat[i] = fftconvolve(ref, stm[::-1,::-1], 'same')
 
-            # find the maximum correlation and add to the dictionary
+            # -- find the maximum correlation and add to the dictionary
             mind          = cc_sub_mat[i].argmax()
-            cc_sub_dic[f] = mind # [mind / blen - bord, mind % blen - bord]
+            off           = [mind / nside - nside/2, mind % nside - nside/2]
 
-        # send sub-matrix back to parent
+            if max(np.abs(off))>(bord-1):
+                print("DST_REGISTER: REGISTRATION FAILED FOR FILE " + 
+                      "{0}!!!".format(os.path.join(p,f)))
+
+                off = [0,0]
+
+            cc_sub_dic[f] = off
+
+        # -- send sub-matrix back to parent
         if multi:
             conn.send([cc_sub_mat,cc_sub_dic])
             conn.close()
@@ -87,15 +101,15 @@ def register(inpath='', infile='', outpath='', outfile='', start='',
             return cc_sub_mat, cc_sub_dic
 
 
-    # calculate the correlation matrix
+    # -- calculate the correlation matrix
     if multi:
         print("DST_REGISTER: running {0} processes...".format(nproc))
 
-        # initialize the full correlation matrix and processes
+        # -- initialize the full correlation matrix and processes
         cc_mat = np.zeros([len(files),2*bord+1,2*bord+1])
         parents, childs, ps = [], [], []
 
-        # initialize the pipes and processes, then start
+        # -- initialize the pipes and processes, then start
         for ip in range(nproc):
             ptemp, ctemp = multiprocessing.Pipe()
             parents.append(ptemp)
@@ -107,7 +121,7 @@ def register(inpath='', infile='', outpath='', outfile='', start='',
                                               kwargs={'verbose':ip==0}))
             ps[ip].start()
 
-        # collect the results, put into cc_mat, and rejoin
+        # -- collect the results, put into cc_mat, and rejoin
         for ip in range(nproc):
             cc_sub_mat, cc_sub_dic = parents[ip].recv()
             cc_mat[dind*ip:dind*(ip+1),:,:] = cc_sub_mat
