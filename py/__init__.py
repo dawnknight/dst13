896395c31cb8566abfda946c780a7902494d3611
Greg Dobler <greg.dobler@gmail.com>
2013-12-07 16:06:02 -0500

    updated

--- py/__init__.py
+++ py/__init__.py
@@ -11,3 +11,4 @@ from dst13.py.dst_imtools import *
 from dst13.py.dst_kmeans_coarse import *
 from dst13.py.dst_night_times import *
 from dst13.py.dst_onoff import *
+from dst13.py.dst_kmeans import *
