164e2040f961e1fbbdd27f5c8948422e00918520
Greg Dobler <greg.dobler@gmail.com>
2013-12-16 10:58:36 -0500

    thresholding modifications

--- py/dst_fit_step.py
+++ py/dst_fit_step.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import *
 
 # -- things to pass
 npix  = 6*5
-width = 0
+width = 6
 
 
 # -- utilities                                                              
@@ -37,15 +37,33 @@ step_r[npix/2:] = 1.0
 
 # -- smooth 
 if width>0:
-    lc     = gaussian_filter(lc,width)
+    for i in (0,1,2):
+        lc[:,i] = gaussian_filter(lc[:,i],width)
+
     tvals  = gaussian_filter(tvals,width)
     step_l = gaussian_filter(step_l,width)
     step_r = gaussian_filter(step_r,width)
 
 
 # -- estimate the noise
-pixfac = 0.532/np.sqrt(float(width)) if width > 0 else 1.0 # correlated noise
-noise = (np.roll(lc,1)-lc)[1:-1].std()/np.sqrt(2.0)/pixfac
+#pixfac = 0.532/np.sqrt(float(width)) if width > 0 else 1.0 # correlated noise
+#noise = (np.roll(lc,1)-lc)[1:-1].std()/np.sqrt(2.0)#/pixfac
+
+dif = (np.roll(lc,1,0)-lc)[1:-1].T#/pixfac
+avg = dif.mean(1)
+sig = dif.std(1)
+
+for _ in (0,1,2):
+    thresh = (avg+5*sig)#.clip(1.0,1e6)
+    w = where((dif[0]<thresh[0]) &
+              (dif[1]<thresh[1]) &
+              (dif[2]<thresh[2]) 
+          )[0]
+    avg = np.array([dif[i][w].mean() for i in (0,1,2)])
+    sig = np.array([dif[i][w].std() for i in (0,1,2)])
+
+noise = sig/np.sqrt(2.0)
+
 
 
 # -- generate the two models and initialize some utilities
@@ -75,8 +93,8 @@ for iband in (0,1,2):
 
         dvals[:] = lc[ii:ii+npix,iband]
 
-        if True in dvals.mask:
-            continue
+#        if True in dvals.mask:
+#            continue
 
         mod1 = np.dot(np.dot(np.dot(tmpl_1,dvals),ptpinv_1),tmpl_1)
         mod2 = np.dot(np.dot(np.dot(tmpl_2,dvals),ptpinv_2),tmpl_2)
@@ -84,17 +102,29 @@ for iband in (0,1,2):
         chisq_1[iband,ii] = ((
             dvals - 
             np.dot(np.dot(np.dot(tmpl_1,dvals),ptpinv_1),tmpl_1))**2
-        ).sum()/(noise**2)/(float(npix)-3)
+        ).sum()/(noise[iband]**2)/(float(npix)-3)
 
         chisq_2[iband,ii] = ((
             dvals - 
             np.dot(np.dot(np.dot(tmpl_2,dvals),ptpinv_2),tmpl_2))**2
-        ).sum()/(noise**2)/(float(npix)-2)
+        ).sum()/(noise[iband]**2)/(float(npix)-2)
 
 
 dif = chisq_2 - chisq_1
 avg = dif.mean(1)
 sig = dif.std(1)
+
+for _ in range(20):
+    thresh = (avg+5*sig)#.clip(1.0,1e6)
+    w = where((dif[0]<thresh[0]) &
+              (dif[1]<thresh[1]) &
+              (dif[2]<thresh[2]) 
+          )[0]
+    avg = np.array([dif[i][w].mean() for i in (0,1,2)])
+    sig = np.array([dif[i][w].std() for i in (0,1,2)])
+    print avg
+    print sig
+
 thresh = (avg+10*sig)#.clip(1.0,1e6)
 
 w = where(
