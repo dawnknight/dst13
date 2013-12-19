718c6af550c5e89707555a52e6f50fcf0002b5dc
Greg Dobler <greg.dobler@gmail.com>
2013-12-19 18:44:15 -0500

    updated

--- py/__init__.py
+++ py/__init__.py
@@ -15,3 +15,4 @@ from dst13.py.dst_kmeans import *
 from dst13.py.dst_plots import *
 from dst13.py.dst_time_ticks import *
 from dst13.py.dst_reference_image import *
+from dst13.py.dst_fit_step import *
