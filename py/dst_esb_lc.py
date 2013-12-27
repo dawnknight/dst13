cf659c976fc20d8b1336ee5932dd76fcd21b2389
Greg Dobler <greg.dobler@gmail.com>
2013-12-27 09:24:49 -0500

    multiple nights

--- py/dst_esb_lc.py
+++ py/dst_esb_lc.py
@@ -13,48 +13,58 @@ fl    = pkl.load(fopen)
 fopen.close()
 
 
-# -- pull out the files for the first night
-paths, files, times = fl.time_slice('11/13/13 19:00:00', '11/14/13 05:00:00')
+# -- get the registration dictionary
+fopen  = open(os.path.join('../output','registration_dictionary.pkl'),'rb')
+cc_dic = pkl.load(fopen)
+fopen.close()
 
 
 # -- set up utilities
 nrow   = lr[0]-ul[0]
 ncol   = lr[1]-ul[1]
 nband  = 3
-ntimes = len(times)
-lc_esb = np.zeros([nband,ntimes])
 bord   = 20
 img    = np.zeros([nrow,ncol,3])
 
-# -- get the registration dictionary
-fopen  = open(os.path.join('../output','registration_dictionary.pkl'),'rb')
-cc_dic = pkl.load(fopen)
-fopen.close()
+
+# -- get the night times
+start, end = dst13.night_times()
    
 
-# -- get the file
-for itime, (p,f,t) in enumerate(zip(cls.paths,cls.files,cls.times)):
+# -- loop through nights
+for inight, (st, en) in enumerate(zip(start,end)):
 
-    print("working on image {0} of {1}...".format(itime+1,ntimes))
+    # -- pull out the files list
+    paths, files, times = fl.time_slice(st,en)
 
-    # -- apply the registration
-    dr, dc = cc_dic[f]
+    ntimes = len(times)
+    lc_esb = np.zeros([nband,ntimes])
 
-    if max(abs(dr),abs(dc))<20:
-        img[:,:,:] = np.roll(
-            np.roll(
-                dst13.read_raw(f,p),dr,0
-                ),dc,1
-            )[ul[0]:lr[0],ul[1]:lr[1],:]
-    else:
-        img[:,:,:] = dst13.read_raw(f,p)[ul[0]:lr[0],ul[1]:lr[1],:]
+    # -- get the file
+    for itime, (p,f,t) in enumerate(zip(paths,files,times)):
+
+        print("working on image " + 
+              "{0} of {1} for night {2}...".format(itime+1,ntimes,inight))
+
+        # -- apply the registration
+        dr, dc = cc_dic[f]
+
+        if max(abs(dr),abs(dc))<20:
+            img[:,:,:] = np.roll(
+                np.roll(
+                    dst13.read_raw(f,p),dr,0
+                    ),dc,1
+                )[ul[0]:lr[0],ul[1]:lr[1],:]
+        else:
+            img[:,:,:] = dst13.read_raw(f,p)[ul[0]:lr[0],ul[1]:lr[1],:]
 
 
-    # -- get the light curve
-    lc_esb[:,0] = img.transpose(2,0,1).reshape(nband,nrow*ncol).mean(1)
+        # -- get the light curve
+        lc_esb[:,itime] = img.transpose(2,0,1
+                                        ).reshape(nband,nrow*ncol).mean(1)
 
 
-# -- write to file
-fopen = open('../output/esb_lc_night_18.pkl')
-pkl.dump(lc_esb,fopen)
-fopen.close()
+    # -- write to file
+    fopen = open('../output/esb_lc_night_'+str(inight).zfill(2)+'_2.pkl','wb')
+    pkl.dump(lc_esb,fopen)
+    fopen.close()
